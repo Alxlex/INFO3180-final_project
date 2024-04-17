38d7924f2a7b3bcf1615777e27e4d975f071897f
Khadeem Lattibeaudiere <khadeemlatti20@gmail.com>
2024-04-16 20:24:51 -0500

    Preliminary forms.py file

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,40 @@
 # Add any form classes for Flask-WTF here
 from flask_wtf import FlaskForm
-from wtforms.validators import InputRequired
-from wtforms import StringField, TextAreaField
+from wtforms.validators import InputRequired, NumberRange
+from wtforms import IntegerField, PasswordField, StringField, TextAreaField
 from flask_wtf.file import FileAllowed, FileRequired, FileField
 
 # class ():
+.3
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
+
+    firstName= StringField('First Name', validators=[InputRequired()])
+    lastname=StringField('Lastname', validators=[InputRequired()])
+    email=StringField('Email', validators=[InputRequired()])
+    location=StringField('Location', validators=[InputRequired()])
+    biography=StringField('Biography', validators=[InputRequired()])
+    
+    profile_photo = FileField('Photo', validators=[
+    FileRequired(),
+    FileAllowed(['jpg', 'png'], 'Images only!')])
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
+
+
+class PostForm(Flaskform):
+    caption=photo =StringField('caption', validators=[InputRequired()])
+    photo = FileField('Photo', validators=[
+    FileRequired(),
+    FileAllowed(['jpg', 'png'], 'Images only!')])
+    user_id=IntegerField('User ID', validators=[InputRequired(), NumberRange(min=1)])
+    #created_on=
+    
+class UploadForm(FlaskForm):
+    photo = FileField('Photo', validators=[
+    FileRequired(),
+    FileAllowed(['jpg', 'png'], 'Images only!')])
